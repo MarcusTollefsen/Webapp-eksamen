0c5cbef41409ce2b93f90e3749b86f196b7b01f1
MarcusTollefsen <113443191+MarcusTollefsen@users.noreply.github.com>
2023-12-04 11:27:39 +0100

    remove

--- oppgave_1/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export type Task = {
-  id: string
-  text: string
-  type: "add" | "divide" | "multiply" | "subtract"
-  data: `${number}|${number}`
-}
-
-export type Type = "add" | "subtract" | "multiply" | "divide"
